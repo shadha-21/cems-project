9b3afd592c19907b2371ec72d4ecacb0cf987073
shadha-21 <fathimashadha398@gmail.com>
2026-03-08 14:12:46 +0530

    fix db_config syntax

--- db_config.py
+++ db_config.py
@@ -3,10 +3,11 @@ import mysql.connector
 import os
 
 def get_db_connection():
-    return mysql.connector.connect(
+    connection = mysql.connector.connect(
         host=os.getenv("mysql.railway.internal"),
         user=os.getenv("root"),
         password=os.getenv("FBhoPiXyjGqBWcnuwxMJdRwGJSAyvYM"),
         database=os.getenv("railway"),
         port=int(os.getenv("3306"))
     )
+    return connection 

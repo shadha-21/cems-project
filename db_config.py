876b83021dfeefa75da2f0ece061e2e06e41c9f6
shadha-21 <fathimashadha398@gmail.com>
2026-03-08 15:18:37 +0530

    fixed db config

--- db_config.py
+++ db_config.py
@@ -1,13 +1,12 @@
-
 import mysql.connector
 import os
 
 def get_db_connection():
     connection = mysql.connector.connect(
-        host=os.getenv("mysql.railway.internal"),
-        user=os.getenv("root"),
-        password=os.getenv("FBhoPiXyjGqBWcnuwxMJdRwGJSAyvYM"),
-        database=os.getenv("railway"),
-        port=int(os.getenv("3306"))
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        port=int(os.getenv("MYSQLPORT"))
     )
-    return connection 
+    return connection

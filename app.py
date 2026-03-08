ec54af895577ba9288ac5a3bfaa5a4edba6e588a
shadha-21 <fathimashadha398@gmail.com>
2026-03-08 17:23:38 +0530

    Fix deployment issues / update db_config

--- app.py
+++ app.py
@@ -687,6 +687,8 @@ def logout():
  #   webbrowser.open_new("http://127.0.0.1:5000")
 import os
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+#if __name__ == "__main__":
+ #   app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)

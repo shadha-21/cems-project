f50891833c6a30ce45c2081a6f3a20944f4f118d
shadha-21 <fathimashadha398@gmail.com>
2026-03-08 14:02:37 +0530

    fixed db_config syntax

--- db_config.py
+++ db_config.py
@@ -1,12 +1,4 @@
 
-#import mysql.connector
-
-#def get_db_connection():
-#    return mysql.connector.connect(
-#        host="localhost",
-#        user="root",
-#        password="Shadha@123",
-#  )
 import mysql.connector
 import os
 
